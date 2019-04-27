2469f1e276ba5f6ebd461384f92148ae71ce9391
Lui Wai Ho <wilsom20012@gmail.com>
2019-04-27 08:06:25 +0800

    debug2

---
+++ static/js/form_submit_ajax.js
@@ -0,0 +1,6 @@
+const element = document.querySelector('form');
+element.addEventListener('submit', event => {
+  event.preventDefault();
+  // actual logic, e.g. validate the form
+  console.log('Form submission cancelled.');
+});
